c89800465ec0bb8a92cf37286090967c7a8cacad
Patrick Byrne <code@patrickbyrne.net>
2009-07-06 14:47:17 -0500

    updated specs to use clearer %Q string syntax (the escaped double quotes were getting a little nuts)

--- spec/array-to-csv_spec.rb
+++ spec/array-to-csv_spec.rb
@@ -1,7 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 # TODO replace the it declarations with more meaningful and descriptive, er, descriptions
-# TODO would heredoc be better and more expressive than a bunch of escaped quotes?
 describe Array do
   context "managing converting itself to CSV format" do
     it "returns '' on an emtpy array" do
@@ -13,25 +12,25 @@ describe Array do
     it "returns 'a\nb\nc' for an array ['a', 'b', 'c']" do
       data = ['a', 'b', 'c']
       
-      data.to_csv.should == "\"a\"\n\"b\"\n\"c\""
+      data.to_csv.should == %Q("a"\n"b"\n"c")
     end
     
     it "returns 'a,b\nc,d' for an array [['a', 'b'], ['c', 'd']]" do
       data = [['a', 'b'], ['c', 'd']]
       
-      data.to_csv.should == "\"a\",\"b\"\n\"c\",\"d\""
+      data.to_csv.should == %Q("a","b"\n"c","d")
     end
     
     it "returns 'a: b\nc: d' for an array [{:a => 'b'}, {:c => 'd'}]" do
       data = [{:a => 'b'}, {:c => 'd'}]
       
-      data.to_csv.should == "\"a: b\"\n\"c: d\""
+      data.to_csv.should == %Q("a: b"\n"c: d")
     end
     
     it "returns 'Patrick,2009-09-13\nAngela,2009-08-17' for an array [['Patrick', Date.new(2009,9,13)], ['Angela', Date.new(2009,8,17)]]" do
       data = [['Patrick', Date.new(2009,9,13)], ['Angela', Date.new(2009,8,17)]]
       
-      data.to_csv.should == "\"Patrick\",\"2009-09-13\"\n\"Angela\",\"2009-08-17\""
+      data.to_csv.should == %Q("Patrick","2009-09-13"\n"Angela","2009-08-17")
     end
   end
 end
