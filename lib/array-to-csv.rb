c72353165612d1748ce0adf83c9102a73958045d
Patrick Byrne <code@patrickbyrne.net>
2009-07-07 14:30:06 -0500

    added code that escaped double-quotes in the cells

--- lib/array-to-csv.rb
+++ lib/array-to-csv.rb
@@ -4,15 +4,23 @@ class Array
     
     for i in self
       # todo research case syntax, since these elsifs are ugly
+      # todo investigate switching to %Q syntax to make less ugly
+      # todo refactor out the duplicate gsubs
       if i.is_a? Array
-        tmp << i.map{|x| "\"#{x}\""}.join(',')
+        tmp << i.map{|x| "\"#{x.to_s.escape_csv_quotes}\""}.join(',')
       elsif i.is_a? String
-        tmp << "\"#{i}\""
+        tmp << "\"#{i.escape_csv_quotes}\""
       elsif i.is_a? Hash
-        tmp << i.map{|k, v| "\"#{k}: #{v}\""}.join(',')
+        tmp << i.map{|k, v| "\"#{k.to_s.escape_csv_quotes}: #{v.to_s.escape_csv_quotes}\""}.join(',')
       end
     end
     
     tmp.join("\n")
+  end
+end
+
+class String
+  def escape_csv_quotes
+    gsub('"','""')
   end
 end
--- spec/array-to-csv_spec.rb
+++ spec/array-to-csv_spec.rb
@@ -32,5 +32,41 @@ describe Array do
       
       data.to_csv.should == %Q("Patrick","2009-09-13"\n"Angela","2009-08-17")
     end
+    
+    context "managing the escaping of double quotes in the cells" do
+      it "correctly escapes double quotes in nested arrays" do
+        data = [["a href=\"foo\"", "div"], ["link rel=\"bar\"","blockquote"]]
+
+        data.to_csv.should == %Q("a href=""foo""","div"\n"link rel=""bar""","blockquote")
+      end
+      
+      it "correctly escapes double quotes in strings" do
+        data = ["a href=\"foo\"", "link rel=\"bar\""]
+        
+        data.to_csv.should == %Q("a href=""foo"""\n"link rel=""bar""")
+      end
+      
+      it "correctly escapes double quotes in hashes" do
+        data = [{ :url => 'a href="foo"', :style => 'link rel="bar"'}]
+        
+        data.to_csv.should == %Q("url: a href=""foo""","style: link rel=""bar""")
+      end
+    end
+  end
+end
+
+describe String do
+  context "handling escaping double quotes in CSV" do
+    it "returns empty string when escaping an empty string" do
+      "".escape_csv_quotes.should == ""
+    end
+    
+    it "returns the origal string if doesn't have any double-quotes" do
+      "foo".escape_csv_quotes.should == "foo"
+    end
+    
+    it "returns two double quotes for every double quote" do
+      'a href="foo"'.escape_csv_quotes.should == 'a href=""foo""'
+    end
   end
 end
